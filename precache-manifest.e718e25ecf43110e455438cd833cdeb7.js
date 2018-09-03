34f349cfa7c93af0c69fcca1076fbe69c9267798
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-09-04 00:06:11 +0900

    deploy

---
+++ precache-manifest.e718e25ecf43110e455438cd833cdeb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sticky-notes/robots.txt"
+  },
+  {
+    "revision": "a4b62284e0e5ff1e67da",
+    "url": "/sticky-notes/js/chunk-vendors.77a24e9c.js"
+  },
+  {
+    "revision": "fa277733aa31f01a9616",
+    "url": "/sticky-notes/js/app.8f05d804.js"
+  },
+  {
+    "revision": "5b081ab16ee2195ee49b3a59b4c7f93a",
+    "url": "/sticky-notes/index.html"
+  },
+  {
+    "revision": "fa277733aa31f01a9616",
+    "url": "/sticky-notes/css/app.dd6e5ccf.css"
+  }
+];
